3f9332259b806a1407067336aa5d4c320c6bde06
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-10-29 02:15:43 +0200

    Updates

--- scripts/game.js
+++ scripts/game.js
@@ -220,7 +220,8 @@ var Play = function (_Preload) {
   _createClass(Play, [{
     key: 'create',
     value: function create() {
-      background = this.add.sprite(0, 0, 'background');
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      background = this.add.tileSprite(0, 0, window.innerWidth, window.innerHeight, 'background');
 
       var scoreFont = { font: "30px Fredoka One", fill: "#fff", align: "center" };
       scoreIcon = this.add.sprite(window.innerWidth - 220, 0, 'score');
@@ -647,7 +648,8 @@ var StartGame = function (_Preload) {
 		value: function create() {
 			var center = { x: this.game.world.centerX, y: this.game.world.centerY };
 
-			background = this.add.sprite(0, 0, 'background');
+			this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+			background = this.add.tileSprite(0, 0, window.innerWidth, window.innerHeight, 'background');
 
 			soundBtn = this.add.button(window.innerWidth - 120, window.innerHeight - 120, 'btnSound', handleMusic, this, 2, 1, 0);
 			soundBtn.width = 100;
